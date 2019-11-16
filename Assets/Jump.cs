6614be9cebf9189b5563af7ee7be5dc8febd7e83
y07014ow <owen.wilson@student.manchester.ac.uk>
2019-11-16 16:58:17 +0000

    Player added, can jump

---
+++ Assets/Jump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private bool isJumping;
+    public int airtime = 60;
+    public float jumpMultiplier;
+    IEnumerator playerJump()
+    {
+        for(int i = 0; i < airtime; i++)
+        {
+            if (i < airtime / 2)
+            {
+                transform.Translate(Vector3.up * jumpMultiplier);
+            }
+            else
+            {
+                transform.Translate(Vector3.up * -1 * jumpMultiplier);
+            }
+            yield return null;
+        }
+        isJumping = false;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            while(isJumping == false)
+            {
+                isJumping = true;
+                StartCoroutine("playerJump");
+            }
+        }
+    }
+}
