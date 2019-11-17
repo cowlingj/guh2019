fd0999b7107a1f3460c8380bf20333aed4805fff
y07014ow <owen.wilson@student.manchester.ac.uk>
2019-11-17 08:14:21 +0000

    Pause Menu added

---
+++ Assets/Menu/PauseGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    private bool paused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (paused == false)
+            {
+                paused = true;
+                Time.timeScale = 0.0f;
+                PauseMenu.SetActive(true);
+            }
+            else if (paused == true)
+            {
+                paused = false;
+                Time.timeScale = 1.0f;
+                PauseMenu.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Menu/RestartFromPause.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartFromPause : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Main Game");
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1.0f;
+    }
+}
